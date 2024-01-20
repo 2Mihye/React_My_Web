f43c0289505b84bbd62ef21988f1c10e293aa93b
Kaye <139112573+2Mihye@users.noreply.github.com>
2024-01-20 23:41:33 +0900

    done

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,12 @@ import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Home from "./component/Home";
 import Movies from "./component/Movie/Movies";
-import TodoList from "./component/Todo/TodoList";
+import TodoList from "./component/TodoList";
 import "./App.css";
 import NumberGuessingGameLimit from "./component/Game/NumberGuessingGameLimit";
 import Quiz from "./component/Game/Quiz";
-import Weather from "./component/Weather";
+import WeatherSearch from "./component/Weather/WeatherSearch";
+import FastClick from "./component/Game/FastClick";
 
 // indexJS가 최상위 js이기 때문에 경로문제로 인해 제일 위에도 bootstrap을 넣어준다.
 function App() {
@@ -25,9 +26,10 @@ function App() {
               path="/NumberGuessingGameLimit"
               element={<NumberGuessingGameLimit />}
             />
+            <Route path="/FastClick" element={<FastClick />} />
             <Route path="/Movies" element={<Movies />} />
-            <Route path="/Todo" element={<TodoList />} />
-            <Route path="/Weather" element={<Weather />} />
+            <Route path="/TodoList" element={<TodoList />} />
+            <Route path="/WeatherSearch" element={<WeatherSearch />} />
           </Routes>
         </div>
         <Footer />
